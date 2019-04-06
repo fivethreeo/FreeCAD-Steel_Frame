023d2f4a8e8952376c865e7a79afaaf5e3ea29ce
polaris <seawind@protonmail.com>
2019-04-05 18:29:33 -0700

    Adds the function to divide the frame into rectangles to panelize

--- funcPanelizer/tools.py
+++ funcPanelizer/tools.py
@@ -16,7 +16,7 @@ class Rectangle(object):
 		# self.despArea=0
 		# self.despPorc=0
 		
-def drawRectangle(rect,steelFrame, flip):
+def drawRectangle(rect,steelFrame,flip):
 	"""
 	Draws the rectangle rect over the steelFrame
 	"""
@@ -43,16 +43,78 @@ def divideFrame(steelFrame,offSets,flip):
 	frame.vertI=FreeCAD.Vector(-offSetLeft,0,-offSets['Bottom'])
 	frame.Length=steelFrame.Length.Value+offSetLeft+offsetRight
 	frame.Height=steelFrame.Height.Value+offSets['Top']+offSets['Bottom']
-	print(frame.vertI)
-	print(frame.Length,frame.Height)
-	drawRectangle(frame,steelFrame,flip)
-	if len(steelFrame.Windows)>0:
-		windows=[list(map(float,w.split(','))) for w in steelFrame.Windows]
-		windows=sorted(windows,key=lambda x:x[0])
+	#drawRectangle(frame,steelFrame,flip)
+	
+	if len(steelFrame.Windows)>0:                           #Gets the information about windows and doors
+		windows=[list(map(float,w.split(','))) for w in steelFrame.Windows]		
+		windows=sorted(windows,key=lambda x:x[0])		
+		for i,w in enumerate(windows):						#Adds the offsets and joints space to the windows and doors
+			of=offSets['Doors'] if w[1]==0 else offSets['Windows']
+			windows[i]=[w[0]+of,w[1] if w[1]==0 else w[1]+of,w[2]-2*of,w[3]-of if w[1]==0 else w[3]-2*of]
 	else:
 		windows=[]
 	
+	j=offSets['Joints'] #Joints space
+	rectangles=[]
 	
-	#def 
+	def createRectangles(fr):
+		"""
+		Recursive function that divides the received rectangle in two rectangles
+		fr: receives an object of the class Rectangle (the frame)
+		"""
+		#windows inside the frame
+		winInt=[w for w in windows if fr.vertI.x<=w[0] and fr.vertI.x+fr.Length>=w[0]+w[2] and fr.vertI.z<=w[1] and fr.vertI.z+fr.Height>=w[1]+w[3]]	
+		winInt=sorted(winInt,key=lambda x:x[0])
+				
+		if len(winInt)>0: #If the frame contains windows
+			if fr.vertI.x<winInt[0][0]: #If there is no window on the left side
+				sFL=Rectangle()  #Subframe on the left side
+				for c in ['x','y','z']:
+					setattr(sFL.vertI,c,getattr(fr.vertI,c))
+				setattr(sFL,'Height',fr.Height)
+				setattr(sFL,'Length',winInt[0][0]-sFL.vertI.x)
+				
+				sFR=Rectangle() #Subframe on the right side
+				for c in ['y','z']:
+					setattr(sFR.vertI,c,getattr(fr.vertI,c))
+				setattr(sFR.vertI,'x',winInt[0][0])
+				setattr(sFR,'Height',fr.Height)
+				setattr(sFR,'Length',fr.Length-sFL.Length)
+				
+				rectangles.append(sFL)
+				createRectangles(sFR)				
+			
+							
+			elif fr.vertI.x==winInt[0][0]: #If there is a window on the left side				
+				sFR=Rectangle() #Subframe on the right side
+				for c in ['y','z']:
+					setattr(sFR.vertI,c,getattr(fr.vertI,c))
+				setattr(sFR.vertI,'x',winInt[0][0]+winInt[0][2])
+				setattr(sFR,'Height',fr.Height)
+				setattr(sFR,'Length',fr.Length-winInt[0][2])
+				createRectangles(sFR)
+				
+				sFT=Rectangle()  #Subframe on the top side
+				for c in ['x','y']:
+					setattr(sFT.vertI,c,getattr(fr.vertI,c))
+				setattr(sFT.vertI,'z',winInt[0][1]+winInt[0][3])
+				setattr(sFT,'Height',fr.vertI.z+fr.Height-sFT.vertI.z)
+				setattr(sFT,'Length',winInt[0][2])
+				createRectangles(sFT)
+				
+				if winInt[0][1]>0:
+					sFB=Rectangle()  #Subframe on the bottom side
+					for c in ['x','y','z']:
+						setattr(sFB.vertI,c,getattr(fr.vertI,c))
+					setattr(sFB,'Height',winInt[0][1]-fr.vertI.z)
+					setattr(sFB,'Length',winInt[0][2])
+					createRectangles(sFB)
+		else:
+			rectangles.append(fr)
+			
+			
 		
 	
+	createRectangles(frame)
+	for r in rectangles:
+		drawRectangle(r,steelFrame,flip)
