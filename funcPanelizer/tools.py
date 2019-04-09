481ae02f4a36bfba07430c3ad1defad38c4e609a
polaris <seawind@protonmail.com>
2019-04-08 17:02:08 -0700

    divideFrame function first fully working version

--- funcPanelizer/tools.py
+++ funcPanelizer/tools.py
@@ -1,4 +1,6 @@
 import os
+from itertools import groupby
+
 import FreeCAD, Draft
 
 #Path to the user Interface
@@ -11,10 +13,7 @@ class Rectangle(object):
 		self.Length=0
 		self.Height=0		
 		self.vertI=FreeCAD.Vector()
-		# self.listaPiezas=[]
-		# self.Label=''
-		# self.despArea=0
-		# self.despPorc=0
+
 		
 def drawRectangle(rect,steelFrame,flip):
 	"""
@@ -30,10 +29,28 @@ def drawRectangle(rect,steelFrame,flip):
 	rotE=steelFrame.Placement.Rotation.toEuler()
 	rot=FreeCAD.Rotation(rotE[0],rotE[1],rotE[2]+90)
 	rect=Draft.makeRectangle(rect.Length, rect.Height, placement=FreeCAD.Placement(place, rot))
+	
+def mergeAdjacentRect(adj):
+	"""
+	Merges the adjacent rectangles received in the list adj
+	Receives the list adj with the adjacent rectangles
+	Returns one rectangle
+	"""
+	adj=sorted(adj,key=lambda x: x.vertI.x)
+	rec=Rectangle()
+	for c in ['x','y','z']:
+		setattr(rec.vertI,c,getattr(adj[0].vertI,c))
+		setattr(rec,'Height',adj[0].Height)
+		setattr(rec,'Length',adj[-1].vertI.x+adj[-1].Length-adj[0].vertI.x)
+	return rec
 		
 def divideFrame(steelFrame,offSets,flip):
 	"""
-	Divides the frame into rectangles 	
+	Divides the frame into rectangles 
+	Parameters:
+		-steelFrame: (obj) the steelFrame object
+		-offsets: (dict) with the offsets
+		-flip: (boolean)
 	"""
 	frame=Rectangle() #Creates a rectangle for the frame
 	offSetLeft=offSets['Left']
@@ -43,7 +60,7 @@ def divideFrame(steelFrame,offSets,flip):
 	frame.vertI=FreeCAD.Vector(-offSetLeft,0,-offSets['Bottom'])
 	frame.Length=steelFrame.Length.Value+offSetLeft+offsetRight
 	frame.Height=steelFrame.Height.Value+offSets['Top']+offSets['Bottom']
-	#drawRectangle(frame,steelFrame,flip)
+	
 	
 	if len(steelFrame.Windows)>0:                           #Gets the information about windows and doors
 		windows=[list(map(float,w.split(','))) for w in steelFrame.Windows]		
@@ -63,7 +80,17 @@ def divideFrame(steelFrame,offSets,flip):
 		fr: receives an object of the class Rectangle (the frame)
 		"""
 		#windows inside the frame
-		winInt=[w for w in windows if fr.vertI.x<=w[0] and fr.vertI.x+fr.Length>=w[0]+w[2] and fr.vertI.z<=w[1] and fr.vertI.z+fr.Height>=w[1]+w[3]]	
+		winInt=[w for w in windows if fr.vertI.x<=w[0] and fr.vertI.x+fr.Length>=w[0]+w[2] and fr.vertI.z<=w[1] and fr.vertI.z+fr.Height>=w[1]+w[3]]
+		#Windows in the middle of two frames
+		winRight=[w for w in windows if fr.vertI.x<=w[0] and w[0]<=fr.vertI.x+fr.Length<w[0]+w[2] and fr.vertI.z<=w[1] and fr.vertI.z+fr.Height>=w[1]+w[3]]
+		for wR in winRight:
+			winInt.append([wR[0],wR[1],fr.vertI.x+fr.Length-wR[0],wR[3]])
+		winLeft=[w for w in windows if w[0]<fr.vertI.x<w[0]+w[2] and fr.vertI.z<=w[1] and fr.vertI.z+fr.Height>=w[1]+w[3]]
+		for wL in winLeft:
+			if wL[0]+wL[2]<fr.vertI.x+fr.Length:
+				winInt.append([fr.vertI.x,wL[1],wL[0]+wL[2]-fr.vertI.x,wL[3]])
+			else:
+				winInt.append([fr.vertI.x,wL[1],fr.Length,wL[3]])
 		winInt=sorted(winInt,key=lambda x:x[0])
 				
 		if len(winInt)>0: #If the frame contains windows
@@ -81,26 +108,28 @@ def divideFrame(steelFrame,offSets,flip):
 				setattr(sFR,'Height',fr.Height)
 				setattr(sFR,'Length',fr.Length-sFL.Length)
 				
-				rectangles.append(sFL)
+				rectangles.append(sFL)				
 				createRectangles(sFR)				
 			
 							
-			elif fr.vertI.x==winInt[0][0]: #If there is a window on the left side				
-				sFR=Rectangle() #Subframe on the right side
-				for c in ['y','z']:
-					setattr(sFR.vertI,c,getattr(fr.vertI,c))
-				setattr(sFR.vertI,'x',winInt[0][0]+winInt[0][2])
-				setattr(sFR,'Height',fr.Height)
-				setattr(sFR,'Length',fr.Length-winInt[0][2])
-				createRectangles(sFR)
+			elif fr.vertI.x==winInt[0][0]: #If there is a window on the left side
+				if fr.Length-winInt[0][2]>0:						
+					sFR=Rectangle() #Subframe on the right side
+					for c in ['y','z']:
+						setattr(sFR.vertI,c,getattr(fr.vertI,c))
+					setattr(sFR.vertI,'x',winInt[0][0]+winInt[0][2])
+					setattr(sFR,'Height',fr.Height)
+					setattr(sFR,'Length',fr.Length-winInt[0][2])
+					createRectangles(sFR)
 				
-				sFT=Rectangle()  #Subframe on the top side
-				for c in ['x','y']:
-					setattr(sFT.vertI,c,getattr(fr.vertI,c))
-				setattr(sFT.vertI,'z',winInt[0][1]+winInt[0][3])
-				setattr(sFT,'Height',fr.vertI.z+fr.Height-sFT.vertI.z)
-				setattr(sFT,'Length',winInt[0][2])
-				createRectangles(sFT)
+				if fr.vertI.z+fr.Height-winInt[0][1]+winInt[0][3]>0:
+					sFT=Rectangle()  #Subframe on the top side
+					for c in ['x','y']:
+						setattr(sFT.vertI,c,getattr(fr.vertI,c))
+					setattr(sFT.vertI,'z',winInt[0][1]+winInt[0][3])
+					setattr(sFT,'Height',fr.vertI.z+fr.Height-sFT.vertI.z)
+					setattr(sFT,'Length',winInt[0][2])
+					createRectangles(sFT)
 				
 				if winInt[0][1]>0:
 					sFB=Rectangle()  #Subframe on the bottom side
@@ -113,8 +142,40 @@ def divideFrame(steelFrame,offSets,flip):
 			rectangles.append(fr)
 			
 			
+			
 		
 	
 	createRectangles(frame)
-	for r in rectangles:
-		drawRectangle(r,steelFrame,flip)
+	#Merge the adjacent rectangles with the same height
+	rect=[]
+	rectangles=sorted(rectangles,key=lambda x:(x.vertI.z, x.Height))
+	groups = groupby(rectangles, lambda x: (x.vertI.z, x.Height))
+	for key, group in groups:
+		g=list(group)
+		if len(g)==1:
+			rect.append(g[0])
+		else:
+			g=sorted(g,key=lambda x: x.vertI.x)
+			adjacent=[]
+			for i in range(len(g)-1):
+				if g[i+1].vertI.x==g[i].vertI.x+g[i].Length:
+					adjacent.append(g[i+1])
+					if g[i] not in adjacent:
+						adjacent.append(g[i])
+				else:					
+					if i+2 == len(g):
+						rect.append(g[i+1])			
+					if len(adjacent)==0:
+						rect.append(g[i])
+					else:
+						rect.append(mergeAdjacentRect(adjacent))
+						adjacent=[]	
+						
+			if len(adjacent)>1:
+				rect.append(mergeAdjacentRect(adjacent))
+				
+	return rect						
+					
+						
+	# for r in rect:
+		# drawRectangle(r,steelFrame,flip)
